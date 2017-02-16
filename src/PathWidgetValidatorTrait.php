f4604132dd72e2dcc46eb2ca4b3e86696d19afbe
Glynn Reid <glynn.reid.gr@gmail.com>
2017-02-16 16:27:13 +0000

    Case 26684; Fix missing alias value in path widget

--- src/PathWidgetValidatorTrait.php
+++ src/PathWidgetValidatorTrait.php
@@ -13,6 +13,7 @@ trait PathWidgetValidatorTrait {
    */
   public static function validateFormElement(array &$element, FormStateInterface $form_state) {
     // Trim the submitted value of whitespace and slashes.
+    $alias = rtrim(trim($element['alias']['#value']), " \\/");
     if (!empty($alias)) {
       $form_state->setValueForElement($element['alias'], $alias);
 
@@ -31,25 +32,24 @@ trait PathWidgetValidatorTrait {
       else if ($domainValues = $form_state->getValue(DOMAIN_ACCESS_FIELD)) {
         // If domains are checked, check existence of alias on each domain.
         foreach ($domainValues as $domainValue) {
-          $domain = \Drupal::service('domain.loader')->load($domainValue['target_id']);
+          //$domain = \Drupal::service('domain.loader')->load($domainValue['target_id']);
           // Validate that the submitted alias does not exist yet.
           $is_exists = \Drupal::service('path.alias_storage')
-            ->setDomainId($domain->getDomainId())
-            ->aliasExists($alias, $element['langcode']['#value'], $element['source']['#value']);
+            ->aliasExistsByDomain($alias, $element['langcode']['#value'], $element['source']['#value'], $domainValue);
           if ($is_exists) {
-            $form_state->setError($element, t('The alias is already in use on :domain.', [':domain' => $domain->get('name')]));
+            $form_state->setError($element, t('The alias is already in use on :domain.', [':domain' => $domainValue]));
           }
         }
       }
-      else {
-        // If no domains are checked and 'all affiliates' is unchecked, check current domain only.
-        $domain = \Drupal::service('domain.loader')->loadDefaultDomain();
-        $is_exists = \Drupal::service('path.alias_storage')
-          ->aliasExists($alias, $element['langcode']['#value'], $element['source']['#value']);
-        if ($is_exists) {
-          $form_state->setError($element, t('The alias is already in use on :domain (default domain).', [':domain' => $domain->get('name')]));
-        }
-      }
+//      else {
+//        // If no domains are checked and 'all affiliates' is unchecked, check current domain only.
+//        $domain = \Drupal::service('domain.loader')->loadDefaultDomain();
+//        $is_exists = \Drupal::service('path.alias_storage')
+//          ->aliasExists($alias, $element['langcode']['#value'], $element['source']['#value']);
+//        if ($is_exists) {
+//          $form_state->setError($element, t('The alias is already in use on :domain (default domain).', [':domain' => $domain->get('name')]));
+//        }
+//      }
 
     }
 
