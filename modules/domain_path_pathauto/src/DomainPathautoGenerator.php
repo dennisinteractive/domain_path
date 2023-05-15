41cad32019d09c943562e56bac420d1069dbb19e
David Ogilo <david.ogilo@dayesoft.co.uk>
2023-05-15 15:58:52 +0100

    [CMS-2193] Added missing file

--- modules/domain_path_pathauto/src/DomainPathautoGenerator.php
+++ modules/domain_path_pathauto/src/DomainPathautoGenerator.php
@@ -8,6 +8,7 @@ use Drupal\Core\Entity\Exception\UndefinedLinkTemplateException;
 use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Render\BubbleableMetadata;
 use Drupal\pathauto\PathautoGenerator;
+use Drupal\pathauto\PathautoGeneratorInterface;
 
 /**
  * Provides methods for generating domain path aliases.
